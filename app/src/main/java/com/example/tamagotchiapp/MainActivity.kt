7b6bbaadd0d16f7a78a47c1d4228c00bf11f30da
CalebRhoda <ST10435099@vcconnect.edu.za>
2024-04-26 16:10:31 +0200

    Finished Project

--- app/src/main/java/com/example/tamagotchiapp/MainActivity.kt
+++ app/src/main/java/com/example/tamagotchiapp/MainActivity.kt
@@ -3,35 +3,16 @@ package com.example.tamagotchiapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var timer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var progressValue = 0
-        val isCountingUp = true
         val startButton = findViewById<Button>(R.id.button)
 
         startButton.setOnClickListener {
-            timer = object : CountDownTimer(Long.MAX_VALUE, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                    if (isCountingUp) {
-                        progressValue++
-                    } else {
-                        progressValue--
-                    }
-
-                    }
-                override fun onFinish() {
-                    // Timer finished
-                }
-
-            }
-            timer.start()
             val intent = Intent(this, PetActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/tamagotchiapp/PetActivity.kt
+++ app/src/main/java/com/example/tamagotchiapp/PetActivity.kt
@@ -1,12 +1,16 @@
 package com.example.tamagotchiapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class PetActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pet)
@@ -16,24 +20,75 @@ class PetActivity : AppCompatActivity() {
         val soapyFoam = findViewById<ImageView>(R.id.imageView4)
         val playButton = findViewById<Button>(R.id.playbutton)
         val tennisBall = findViewById<ImageView>(R.id.imageView5)
+        val hungerCount = findViewById<TextView>(R.id.hungerText)
+        val washCount = findViewById<TextView>(R.id.cleanText)
+        val playCount = findViewById<TextView>(R.id.happyText)
+        val millisInFuture = 10000L
+        val countDownInterval = 1000L
+
+        val hungerCountDown = object : CountDownTimer(millisInFuture, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsRemaining = millisUntilFinished / 1000
+                hungerCount.text = ("$secondsRemaining")
+            }
+
+            override fun onFinish() {
+            }
+
+
+        }
+        val washCountDown = object : CountDownTimer(millisInFuture, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsRemaining = millisUntilFinished / 1000
+                washCount.text = ("$secondsRemaining")
+            }
+
+            override fun onFinish() {
+            }
+
+
+        }
+        val playCountDown = object : CountDownTimer(millisInFuture, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsRemaining = millisUntilFinished / 1000
+                playCount.text = ("$secondsRemaining")
+            }
+
+            override fun onFinish() {
+                // This method is called when the countdown finishes
+            }
+
+        }
+
+
+
 
         feedButton.setOnClickListener {
             if (foodBowl.visibility == View.INVISIBLE) {
                 foodBowl.visibility = View.VISIBLE
                 soapyFoam.visibility = View.INVISIBLE
                 tennisBall.visibility = View.INVISIBLE
+                hungerCount.text = "10"
+                hungerCountDown.cancel()
             } else {
                 foodBowl.visibility = View.INVISIBLE
+                hungerCountDown.cancel()
+                hungerCountDown.start()
             }
 
         }
+
         washButton.setOnClickListener {
             if (soapyFoam.visibility == View.INVISIBLE) {
                 soapyFoam.visibility = View.VISIBLE
                 tennisBall.visibility = View.INVISIBLE
                 foodBowl.visibility = View.INVISIBLE
+                washCount.text = "10"
+                washCountDown.cancel()
             } else {
                 soapyFoam.visibility = View.INVISIBLE
+                washCountDown.cancel()
+                washCountDown.start()
             }
         }
         playButton.setOnClickListener {
@@ -41,11 +96,22 @@ class PetActivity : AppCompatActivity() {
                 tennisBall.visibility = View.VISIBLE
                 foodBowl.visibility = View.INVISIBLE
                 soapyFoam.visibility = View.INVISIBLE
+                playCount.text = "10"
+                playCountDown.cancel()
             } else {
                 tennisBall.visibility = View.INVISIBLE
+                playCountDown.cancel()
+                playCountDown.start()
             }
+
+
         }
-
+        playCountDown.start()
+        washCountDown.start()
+        hungerCountDown.start()
     }
-
 }
+
+
+
+
